0fc6559721a900877f3bf0d3949ec6ad48ff574c
Vo <tarnoga@gmail.com>
2012-11-08 17:12:58 +0400

    Add config

--- batch_ex/batch_ex.py
+++ batch_ex/batch_ex.py
@@ -8,6 +8,7 @@ import gtk
 from gimpfu import *
 from gtkcodebuffer import CodeBuffer, SyntaxLoader, add_syntax_path
 import pango
+from ConfigParser import ConfigParser
 
 # test:
 from pdb import set_trace
@@ -18,6 +19,17 @@ class BatchCodeExec:
        
     def __init__(self):
         _path = path.dirname(sys.modules[self.__module__].__file__)
+
+        _conf_filename = _path + '/config'
+        _conf_dict = {
+            'Menu': {'Layer': 'gimp-layer*'},
+            'Templates': {'Get first layer': 'layer = image.layers[0]'}
+            }
+        if not(path.isfile(_conf_filename)):
+            self._create_conf_file(_conf_filename, _conf_dict)
+        self.config = ConfigParser()
+        self.config.read(_conf_filename) 
+        self._check_conf(_conf_filename, _conf_dict)       
 
         self.ui = gtk.Builder()
         self.ui.add_from_file(_path+'/batch_ex.ui')
@@ -33,6 +45,9 @@ class BatchCodeExec:
         self._add_filters()
         self._get_macro_list()
         self._ckey = ""
+        #Menu
+        self._create_menu()
+
         #colors
         self.red_color = pango.AttrList()
         self.red_color.insert(pango.AttrForeground(65535, 0, 0, 0, -1))
@@ -45,6 +60,71 @@ class BatchCodeExec:
         self.blue_color.insert(pango.AttrWeight(pango.WEIGHT_HEAVY, 0, -1))
 
         self._set_status("New")
+
+    #Create config file
+    def _create_conf_file(self, filename, conf_dict):
+        config = ConfigParser()
+        sections = conf_dict.keys()
+        sections.sort()
+        for section in sections:
+            config.add_section(section)
+            for option in conf_dict[section]:
+                config.set(section, option, conf_dict[section][option])
+        config.write(open(filename,'w'))     
+
+    #Check and fix config
+    def _check_conf(self, filename, conf_dict):
+        sections = conf_dict.keys()
+        sections.sort()
+        for section in sections:
+            if section not in self.config.sections():
+                self.config.add_section(section)
+                for option in conf_dict[section]:
+                    self.config.set(section, option, conf_dict[section][option])
+        self.config.write(open(filename,'w'))            
+  
+    #Меню редактора
+    def _create_menu(self):
+        menu = self.ui.get_object('code_menu')
+
+        #Меню функций PDB
+        pdb_menuitem = gtk.MenuItem('PDB')
+        pdb_menu = gtk.Menu()
+        for item in self.config.options('Menu'):           
+            menuitem = gtk.MenuItem(item)
+            submenu = gtk.Menu()
+            pdb_list = pdb.query(self.config.get('Menu', item))
+            pdb_list.sort()
+            for pdb_item in pdb_list:
+                pdb_proc = pdb[pdb_item]
+                submenu_item = gtk.MenuItem(pdb_item)
+                submenu_item.set_tooltip_markup(
+                    '<b>' + pdb_proc.proc_blurb + '</b>\n'\
+                    + pdb_proc.proc_help)
+                submenu.append(submenu_item)
+                submenu_item.connect('activate', self.add_code, pdb_item +\
+                    '(' + reduce(lambda res,x: res+x[1]+', ',\
+                    pdb_proc.params, '')[:-2] + ')')
+                submenu_item.show()
+            menuitem.set_submenu(submenu)
+            pdb_menu.add(menuitem)
+            menuitem.show()
+        pdb_menuitem.set_submenu(pdb_menu)
+        menu.add(pdb_menuitem)
+        pdb_menuitem.show()
+
+        #Меню шаблонов
+        template_menuitem = gtk.MenuItem('Templates')
+        template_menu = gtk.Menu()
+        for item in self.config.options('Templates'):           
+            submenu_item = gtk.MenuItem(item)
+            submenu_item.connect('activate', self.add_code,
+                self.config.get('Templates', item)) 
+            template_menu.append(submenu_item)
+            submenu_item.show()               
+        template_menuitem.set_submenu(template_menu)
+        menu.add(template_menuitem)
+        template_menuitem.show()        
 
     #Изменение статуса редактора
     def _set_status(self, status):
@@ -72,8 +152,8 @@ class BatchCodeExec:
         self.code = code_buffer.get_text(code_buffer.get_start_iter(),
             code_buffer.get_end_iter())     
 
-    #Выполнение кода (используется переменная img)
-    def _ex_code(self, img):
+    #Выполнение кода (используется переменная image)
+    def _ex_code(self, image):
         try:
             exec(self.code)
         except Exception, error:
@@ -81,15 +161,15 @@ class BatchCodeExec:
 
     #Сохранение файла
     def _save_img(self, img):
-        if img.layers.__len__() > 1:
+        format = self.ui.get_object('format_combo').get_active()        
+        if (format != 2) and (img.layers.__len__() > 1):
             pdb.gimp_image_flatten(img)
         layer = img.layers[0]
         root_filename = path.splitext(path.basename(img.filename))[0]
-        format = self.ui.get_object('format_combo').get_active()
         filename = path.join(
             self.ui.get_object('dir_select').get_filename(),
             path.splitext(path.basename(img.filename))[0] \
-            + ('.jpg', '.tif')[format])
+            + ('.jpg', '.tif', '.xcf')[format])
         #JPG
         if format == 0:
             compress = self.ui.get_object('hscale1').get_value()
@@ -99,6 +179,9 @@ class BatchCodeExec:
         elif format == 1:
             compress = int(self.ui.get_object('checkbutton1').get_active())
             pdb.file_tiff_save(img, layer, filename, filename, compress)
+        #XCF
+        elif format == 2:
+            pdb.gimp_xcf_save(0, img, layer, filename, filename)
 
     #Заполнение списка
     def _get_macro_list(self):
@@ -143,13 +226,21 @@ class BatchCodeExec:
         else:
             return '00000'
 
+    #Выбор процедуры из меню
+    def add_code(self, widget, insert_code):
+        code_buffer = self.ui.get_object('code').get_buffer()
+        code_buffer.insert_at_cursor(insert_code)
+
     #Код в редакторе изменен
     def code_changed(self, widget):
         if self.status.get_text()=='Saved':
             self._set_status('Edited')
 
     def key_press(self, widget, event):
-        if (event.state & gtk.gdk.CONTROL_MASK)\
+        if widget == self.ui.get_object('entry_descr')\
+        and gtk.gdk.keyval_name(event.keyval) == 'Return':
+            self.ui.get_object('code').grab_focus()
+        elif (event.state & gtk.gdk.CONTROL_MASK)\
         and gtk.gdk.keyval_name(event.keyval) == 's':
             self.code_save(widget)
 
@@ -203,10 +294,9 @@ class BatchCodeExec:
 
     #По кнопке Ok: получение файлов, цикл по ним
     def do_it(self, widget):
-        set_trace()
         self._get_code()
         filenames = self.ui.get_object('file_chooser').get_filenames()
-        self.ui.get_object('notebook2').set_current_page(2)  
+        self.ui.get_object('notebook2').set_current_page(3)  
         file_count = filenames.__len__()
         file_num = 0    
         for filename in filenames:
@@ -217,10 +307,10 @@ class BatchCodeExec:
                 + ' ('+str(file_num)+ '/'+str(file_count)+')')
             while gtk.events_pending():
                gtk.main_iteration(False)                        
-            img = pdb.gimp_file_load(filename, filename)
-            self._ex_code(img)   
-            self._save_img(img)
-            pdb.gimp_image_delete(img)  
+            image = pdb.gimp_file_load(filename, filename)
+            self._ex_code(image)   
+            self._save_img(image)
+            pdb.gimp_image_delete(image)  
         self.format_changed(self.ui.get_object('format_combo'))
 
     # Закрытие приложения
